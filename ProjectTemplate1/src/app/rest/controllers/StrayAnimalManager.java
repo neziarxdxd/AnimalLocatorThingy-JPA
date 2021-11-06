81685d485edd3d7c5646ec7361e6d7027ea209e4
raiiiz <rickdavidsangalang@gmail.com>
2021-11-06 21:15:23 +0800

    added animal stray

--- ProjectTemplate1/src/app/rest/controllers/StrayAnimalManager.java
+++ ProjectTemplate1/src/app/rest/controllers/StrayAnimalManager.java
@@ -1,5 +1,38 @@
 package app.rest.controllers;
 
+import javax.ws.rs.Consumes;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import app.components.StraySightLocationComponent;
+import app.entity.StraySightLocation;
+
+
+
+@Component
+@Path("/sighting")
 public class StrayAnimalManager {
+	
+	@Autowired
+	StraySightLocationComponent straySightComponent;
+
+
+	@POST
+	@Path("/new")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test(StraySightLocation data) {
+		return straySightComponent.addStraySight(data);
+	}
+//	public StraySightLocation addition()
+//	{
+//		
+//	}
 
 }
